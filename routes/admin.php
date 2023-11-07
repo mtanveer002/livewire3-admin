045f1cf51250934b6a1162ad3a2f9225afe98f04
Muhammad Tanveer <mtanveerraj002@gmail.com>
2023-11-07 22:49:46 +0500

    code refactor

--- routes/admin.php
+++ routes/admin.php
@@ -17,13 +17,13 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:admin')->group(function () {
     Route::get('dashboard', DashboardController::class)->name('dashboard');
 
-    Route::prefix('cms')->middleware('permission:cms')->as('cms.')->group(function () {
-        Route::middleware('permission:menu:manage-menus')->group(function () {
+    Route::prefix('cms')->as('cms.')->group(function () {
+        Route::middleware('auth:admin')->group(function () {
             Route::get('menus', MenuIndexController::class)->name('menu.index');
             Route::get('menus/{menu}/builder', MenuBuilderController::class)->name('menu.builder');
         });
 
-        Route::middleware('permission:cms:manage-pages')->group(function () {
+        Route::middleware('auth:admin')->group(function () {
             Route::get('pages', PageIndexController::class)->name('pages.index');
             Route::get('pages/create', PageCreateController::class)->name('pages.create');
             Route::get('pages/{page}', PageShowController::class)->name('pages.show');
@@ -32,14 +32,14 @@ Route::middleware('auth:admin')->group(function () {
 
     });
 
-    Route::prefix('system')->middleware('permission:system')->as('system.')->group(function () {
-        Route::middleware('permission:system:manage-staff')->group(function () {
+    Route::prefix('system')->middleware('auth:admin')->as('system.')->group(function () {
+        Route::middleware('auth:admin')->group(function () {
             Route::get('staff', StaffIndexController::class)->name('staff.index');
             Route::get('staff/create', StaffCreateController::class)->name('staff.create');
             Route::get('staff/{staff}', StaffShowController::class)->name('staff.show');
         });
 
-        Route::middleware('permission:system:settings')->group(function () {
+        Route::middleware('auth:admin')->group(function () {
             Route::get('settings', SettingsController::class)->name('settings');
         });
     });
